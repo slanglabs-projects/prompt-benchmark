6fd8413213b7322efd02aa7ad2a9eeed46a11e28
revanth-slanglabs <revanth@slanglabs.in>
2024-08-16 18:16:17 +0530

    [Hiding Admin Panel]

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,12 @@ def main():
     st.set_page_config(page_title="ELO Scoring Platform", layout="wide")
 
     with st.sidebar:
-        selected = option_menu("Navigation", ["ELO Scoring Platform", "Admin Panel", "Leaderboard"],
-                               icons=["house", "gear", "trophy"], menu_icon="cast", default_index=0)
+        selected = option_menu("Navigation", ["ELO Scoring Platform", "Leaderboard"],
+                               icons=["house", "trophy"], menu_icon="cast", default_index=0)
 
     if selected == "ELO Scoring Platform":
         from client.app import main as app_main
         app_main()
-    elif selected == "Admin Panel":
-        from client.admin import main as admin_main
-        admin_main()
     elif selected == "Leaderboard":
         from client.leaderboard import main as leaderboard_main
         leaderboard_main()
