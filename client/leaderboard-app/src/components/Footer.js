5f15173bbff0bc86fb86cc15bc37f37a888d6d25
Slang Labs <slanglabs@Sahis-MacBook-Air.local>
2024-10-07 14:53:55 +0530

    arenaUI

--- client/leaderboard-app/src/components/Footer.js
+++ client/leaderboard-app/src/components/Footer.js
@@ -3,9 +3,11 @@ import { FaGithub } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => (
-  <footer className="bg-dark text-white py-2 text-center">
-    <p>&copy; {new Date().getFullYear()} Slang Labs. All Rights Reserved.</p>
-    <p>
+  <footer className="footer bg-dark text-white py-2 text-center">
+    <p className="footer-text">
+      &copy; {new Date().getFullYear()} Slang Labs. All Rights Reserved.
+    </p>
+    <p className="footer-text">
       <a
         href="https://github.com/slanglabs-projects/prompt-benchmark"
         target="_blank"
